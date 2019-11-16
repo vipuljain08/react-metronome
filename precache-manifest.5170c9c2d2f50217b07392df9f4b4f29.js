c56f3f0d46aa21281a2445efbc20ebd3dd225331
vipuljain08 <vupuljain2001@gmail.com>
2019-11-15 20:18:56 -0800

    Updates

---
+++ precache-manifest.5170c9c2d2f50217b07392df9f4b4f29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ec36049962e4e5c87a4319cf4e8821",
+    "url": "/react-metronome/index.html"
+  },
+  {
+    "revision": "cd6b08b560af207b584a",
+    "url": "/react-metronome/static/css/main.0687a18e.chunk.css"
+  },
+  {
+    "revision": "12da6520505b85b0251f",
+    "url": "/react-metronome/static/js/2.8100f206.chunk.js"
+  },
+  {
+    "revision": "cd6b08b560af207b584a",
+    "url": "/react-metronome/static/js/main.b5882d0f.chunk.js"
+  },
+  {
+    "revision": "d17bc6743b70049adf3d",
+    "url": "/react-metronome/static/js/runtime-main.191aced4.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/react-metronome/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/react-metronome/static/media/click2.fe0ded65.wav"
+  }
+]);
